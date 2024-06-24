c0f03a6272e632bc804a910a899a2589dd25df3a
unknown <maxgate>
2024-06-24 16:00:54 +0100

    processing

--- src/FriendsList.jsx
+++ src/FriendsList.jsx
@@ -1,6 +1,11 @@
-
-const FriendsList = ({ name, image }) => {
-  return <div className="text-center"></div>;
+import profilepix from './assets/profile.png'
+const FriendsList = () => {
+  return <div className="text-center">
+    <div className="flex border border-gray-300 hover:bg-gray-600">
+        <img src={profilepix} alt="friends name" className="rounded-full h-10" />
+        <h3>Max vincent</h3>
+    </div>
+  </div>;
 };
 
 export default FriendsList;
